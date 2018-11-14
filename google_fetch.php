49c0054ce82560dfd68c1115e1d5ffd469a534eb
Ole Stubberud <ole.stubberud@gmail.com>
2018-11-14 16:55:14 +0100

    updated google_fetch.php

--- google_fetch.php
+++ google_fetch.php
@@ -14,23 +14,21 @@ $SEARCH_TYPE = array(
                 "gym",);
 $RANK_BY = "prominence";
 
+
 function searchLatLong($place, $lat, $long, $search_type) {
 
     global $KEY, $SEARCH, $RADIUS, $BASE_URL, $RESULT_TYPE, $RANK_BY;
     
     $url = "$BASE_URL/nearbysearch/$RESULT_TYPE?location=$lat,$long&radius=$RADIUS&type=$search_type&key=$KEY&region=no&language=no&rankby=$RANK_BY";
-    //echo "URL: " .  $url;
     
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);    
     $response = curl_exec($ch);
     curl_close($ch);
 
     $response_a = json_decode($response);
-    $hasResults = (sizeof($response_a->results) > 0); 
+    $hasResults = sizeof($response_a->results) > 0;
 
     if ($hasResults) {
         $place_text = "-----------------------------------------------------------------------------------------------------------\n"; 
@@ -39,19 +37,40 @@ function searchLatLong($place, $lat, $long, $search_type) {
         writeFile($place_text);
     }
 
-    foreach ($response_a->results as &$value) {
+    foreach ($response_a->results as &$result) {
         
         $continue = true;
 
-        $name = $value->name;
-        $vicinity = $value->vicinity;
-        $rating = $value->rating;
-        $types = $value->types;
+        $name = $result->name;
+        $vicinity = $result->vicinity;
+        if (!empty($result->rating)) $rating = $result->rating; 
+        if (!empty($result->types)) $types = $result->types;
 
+        // remove types: school, dentist
+        $exclude_types = array("school", "dentist", "doctor");
+        foreach ($exclude_types as &$exclude) {
+            if(in_array($exclude, $types)) $continue = false;
+        }
+
+        // remove weird names: emails (+ names with only Oslo)
+        $exclude_names = array("@gmail", "@hotmail");
         if($name == "Oslo") $continue = false;
+        foreach ($exclude_names as &$exclude) {
+            if($name == $exclude) $continue = false;
+        }
+
+        // check if returned types has one of 1st or 2nd returned types as our searched type
+        // otherwise we get weird results - like gas station as cafe since they have food
+        if ( !empty($types) && !empty($types[0]) && strcmp($types[0], $search_type) == 0 || 
+             !empty($types) && !empty($types[1]) && strcmp($types[1], $search_type) == 0 ) {
+            $continue = true;
+        } else {
+            $continue = false;
+        }
+        
 
         if($continue) {
-            $result = "Name: " . $name;
+            $result = "Name: " . $name;    
             $result .= " | Address: " . $vicinity;
             if (!empty($rating)) $result .= " | Rating: " . $rating;
             $result .= " | Types: [ " . implode(", ", $types) . " ]";
@@ -61,7 +80,7 @@ function searchLatLong($place, $lat, $long, $search_type) {
         }
     }
 
-    if($hasResults) {
+    if($hasResults && $continue) {
         $place_text = "-----------------------------------------------------------------------------------------------------------\n\n\n";
         writeFile($place_text);
     }
@@ -76,8 +95,6 @@ function getLatLongFromName($place) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     $response = curl_exec($ch);
     curl_close($ch);
 
@@ -100,7 +117,7 @@ function parseFile($filename) {
     try {
         $file = new SplFileObject($filename);
     } catch (LogicException $exception) {
-        die('SplFileObject : '.$exception->getMessage());
+        die('SpleFileObject() function : '.$exception->getMessage());
     }
     while ($file->valid()) {
         $place = $file->fgets();
